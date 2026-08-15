87a84182e1646b7be7b7cb42639219ead7c6cd8f
Nico Mendoza <nico1017mendoza@gmail.com>
2026-08-15 13:39:46 -0500

    ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -18,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
+   public function boot(): void
+{
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
     }
+}
 }
